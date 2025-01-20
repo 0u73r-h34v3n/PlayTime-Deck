f1f3fdf5ea0ec617e2607893e27365d4f9ba8c29
ynhhoJ <22500212+ynhhoJ@users.noreply.github.com>
2025-01-20 18:17:31 +0200

    types: Add `YearlyStatistics`, `Session` & `OverallGameTime`

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,8 +1,30 @@
 type Nullable<T> = T | null | undefined;
 
+declare module "*.css";
+
 interface Unregisterable {
 	/**
 	 * Unregister the callback.
 	 */
 	unregister(): void;
+}
+
+interface YearlyStatistics {
+	month: number;
+	month_name: string;
+	total: number;
+	sessions_count: number;
+	sessions: Array<Session>;
+}
+
+interface Session {
+	date: string;
+	duration: number;
+	migrated?: string;
+}
+
+interface OverallGameTime {
+	id: string;
+	name: string;
+	time: number;
 }
